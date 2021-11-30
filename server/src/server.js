55d2299f32ebe03c17bfcf755f3cf2521fd1b2e6
Veselin Tonev <veselinn16@yahoo.com>
2021-12-01 00:07:25 +0200

    Setup Http server with express

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,22 @@
+import express from 'express'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { makeExecutableSchema } from 'graphql-tools';
-import { graphql } from 'graphql';
 
 const typeDefs = `
 schema {
   query: Query
 }
+# this is how you add a comment
 type Query {
+  # comment for this field is possible
   hello: String
+  """
+  This is how
+  a
+  multi-line comment is made
+  """
   name: String
 }
 `;
@@ -20,8 +30,19 @@ const resolvers = {
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const query = process.argv[2];
+// create server
+const app = express();
 
-graphql(schema, query).then(result => {
-  console.log(JSON.stringify(result, null, 2));
-});
+// add CORS support
+app.use(cors());
+
+// this route will be handled by the following middleware
+// graphiqlExpress needs to be passed the schema
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// second param is cb for when server starts
+app.listen(4000, () => {
+  console.log('Go to http://localhost:4000/graphiql')
+})
